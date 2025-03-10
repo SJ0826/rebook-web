2eae8ee32b101bedf71fb61cd7b3cc34a241dcd7
SJ0826 <ikosdu60@gmail.com>
2025-03-10 15:47:48 +0900

    chore: prettier, eslint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import tseslint from "typescript-eslint";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next', 'prettier', 'eslint:recommended', "next/typescript"],
+  })
 ];
 
 export default eslintConfig;
