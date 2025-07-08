2df76313704e2ed8063a460f3c570af2fb35927e
SJ0826 <ikosdu60@gmail.com>
2025-07-08 18:49:22 +0900

    refactor: 헤더 UI 개선

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,146 +8,150 @@ import { ROUTES } from '@/lib/constants';
 import { useAuth } from '@/hooks/useAuth';
 import Link from 'next/link';
 import LogoutModal from '@/components/modal/LogoutModal';
-import { useToast } from '@/lib/contexts/ToastContext';
 import { noAuthRoutes } from '@/lib/data/noAuthRoutes';
+import { Input } from '@/components/ui';
+import HeaderButton from '@/components/home/HeaderButton';
+import {
+  BookOpenIcon,
+  BuildingStorefrontIcon,
+  ChatBubbleOvalLeftIcon,
+} from '@heroicons/react/24/outline';
 
 export default function Header() {
+  const router = useRouter();
   const pathname = usePathname();
+
   const { logout, isLoggedIn } = useAuth();
-  const { showToast } = useToast();
-  const hideHeader = noAuthRoutes.includes(pathname);
+
   const [isOpenMobileNav, setIsOpenMobileNav] = useState(false);
   const [showLogoutModal, setShowLogoutModal] = useState(false);
-  const router = useRouter();
-
   const [isMounted, setIsMounted] = useState(false);
+
+  const hideHeader = noAuthRoutes.includes(pathname);
+
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  const handleLoginRoute = () => {
-    if (isLoggedIn) {
-      setShowLogoutModal(true);
-      return;
-    }
-    router.push(ROUTES.LOGIN);
-  };
-
-  // 로그인 여부에 따라 이동 or 토스트
-  const handleRouteWithAuth = (route: string) => {
-    if (isLoggedIn) {
-      router.push(route);
-    } else {
-      showToast('로그인이 필요한 서비스입니다.', 'info');
-      router.push(ROUTES.LOGIN);
-    }
-  };
 
   if (hideHeader || !isMounted) return null;
 
   return (
     <>
-      <header className="navbar bg-base-100 fixed top-0 z-50 w-full shadow-md">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl" href={ROUTES.HOME}>
+      <header className="fixed top-0 z-50 w-full py-8 shadow-md">
+        <div className="mx-auto flex max-w-5xl items-center justify-between">
+          {/* 로고 */}
+          <Link className="btn btn-ghost text-xl" href={ROUTES.HOME} prefetch>
             <Image
-              width={46}
-              height={46}
+              width={100}
               src={logoImage}
               alt={'Rebook logo'}
               style={{ width: 'auto', height: 'auto' }}
             />
-            <span className={'hidden md:flex'}>
-              Rebook: 다시 읽고 다시 나누다
-            </span>
-          </a>
+          </Link>
+          <Input className={'w-110'} />
+          <nav className="flex items-center gap-4">
+            <HeaderButton
+              title={'판매하기'}
+              path={ROUTES.BOOK}
+              icon={<BuildingStorefrontIcon />}
+            />
+            <HeaderButton
+              title={'내 서점'}
+              path={ROUTES.BOOK_REGISTER}
+              icon={<BookOpenIcon />}
+            />
+            <HeaderButton
+              title={'리북 톡'}
+              path={ROUTES.BOOK_REGISTER}
+              icon={<ChatBubbleOvalLeftIcon />}
+            />
+          </nav>
+          {/*<nav className="hidden items-center gap-6 md:flex">*/}
+          {/*  <Link*/}
+          {/*    href={'#'}*/}
+          {/*    onClick={(e) => {*/}
+          {/*      e.preventDefault();*/}
+          {/*      handleRouteWithAuth(ROUTES.BOOK_REGISTER);*/}
+          {/*    }}*/}
+          {/*    className="w-20 text-center font-semibold"*/}
+          {/*  >*/}
+          {/*    책 등록*/}
+          {/*  </Link>*/}
+          {/*  <Link*/}
+          {/*    href={'#'}*/}
+          {/*    onClick={(e) => {*/}
+          {/*      e.preventDefault();*/}
+          {/*      handleRouteWithAuth(ROUTES.MY_BOOKSHELF);*/}
+          {/*    }}*/}
+          {/*    className="w-20 text-center font-semibold"*/}
+          {/*  >*/}
+          {/*    나의 서재*/}
+          {/*  </Link>*/}
+          {/*  <Link*/}
+          {/*    href={'#'}*/}
+          {/*    onClick={(e) => {*/}
+          {/*      e.preventDefault();*/}
+          {/*      handleRouteWithAuth(ROUTES.CHAT);*/}
+          {/*    }}*/}
+          {/*    className="w-20 text-center font-semibold"*/}
+          {/*  >*/}
+          {/*    채팅*/}
+          {/*  </Link>*/}
+          {/*  <button*/}
+          {/*    onClick={handleLoginRoute}*/}
+          {/*    className="w-20 text-center font-semibold"*/}
+          {/*  >*/}
+          {/*    {isLoggedIn ? '로그아웃' : '로그인'}*/}
+          {/*  </button>*/}
+          {/*</nav>*/}
+          {/*<button*/}
+          {/*  className="btn btn-ghost md:hidden"*/}
+          {/*  onClick={() => setIsOpenMobileNav((prev) => !prev)}*/}
+          {/*>*/}
+          {/*  ☰*/}
+          {/*</button>*/}
         </div>
 
-        <nav className="hidden items-center gap-6 md:flex">
-          <Link
-            href={'#'}
-            onClick={(e) => {
-              e.preventDefault();
-              handleRouteWithAuth(ROUTES.BOOK_REGISTER);
-            }}
-            className="w-20 text-center font-semibold"
-          >
-            책 등록
-          </Link>
-          <Link
-            href={'#'}
-            onClick={(e) => {
-              e.preventDefault();
-              handleRouteWithAuth(ROUTES.MY_BOOKSHELF);
-            }}
-            className="w-20 text-center font-semibold"
-          >
-            나의 서재
-          </Link>
-          <Link
-            href={'#'}
-            onClick={(e) => {
-              e.preventDefault();
-              handleRouteWithAuth(ROUTES.CHAT);
-            }}
-            className="w-20 text-center font-semibold"
-          >
-            채팅
-          </Link>
-          <button
-            onClick={handleLoginRoute}
-            className="w-20 text-center font-semibold"
-          >
-            {isLoggedIn ? '로그아웃' : '로그인'}
-          </button>
-        </nav>
-        <button
-          className="btn btn-ghost md:hidden"
-          onClick={() => setIsOpenMobileNav((prev) => !prev)}
-        >
-          ☰
-        </button>
-
-        {isOpenMobileNav && (
-          <div className="absolute top-16 right-4 flex w-40 flex-col gap-2 rounded-md bg-white p-3 shadow-lg md:hidden">
-            <a onClick={handleLoginRoute} className="btn btn-ghost">
-              {isLoggedIn ? '로그아웃' : '로그인'}
-            </a>
-            <Link
-              href={'#'}
-              className="btn btn-ghost"
-              onClick={(e) => {
-                e.preventDefault();
-                handleRouteWithAuth(ROUTES.BOOK_REGISTER);
-                setIsOpenMobileNav(false);
-              }}
-            >
-              책 등록
-            </Link>
-            <Link
-              href={'#'}
-              className="btn btn-ghost"
-              onClick={(e) => {
-                e.preventDefault();
-                handleRouteWithAuth(ROUTES.MY_BOOKSHELF);
-                setIsOpenMobileNav(false);
-              }}
-            >
-              나의 서재
-            </Link>
-            {/*<Link*/}
-            {/*  href={'#'}*/}
-            {/*  className="btn btn-ghost"*/}
-            {/*  onClick={(e) => {*/}
-            {/*    e.preventDefault();*/}
-            {/*    handleRouteWithAuth(ROUTES.PROFILE);*/}
-            {/*    setIsOpenMobileNav(false);*/}
-            {/*  }}*/}
-            {/*>*/}
-            {/*  내 정보*/}
-            {/*</Link>*/}
-          </div>
-        )}
+        {/*{isOpenMobileNav && (*/}
+        {/*  <div className="absolute top-16 right-4 flex w-40 flex-col gap-2 rounded-md bg-white p-3 shadow-lg md:hidden">*/}
+        {/*    <a onClick={handleLoginRoute} className="btn btn-ghost">*/}
+        {/*      {isLoggedIn ? '로그아웃' : '로그인'}*/}
+        {/*    </a>*/}
+        {/*    <Link*/}
+        {/*      href={'#'}*/}
+        {/*      className="btn btn-ghost"*/}
+        {/*      onClick={(e) => {*/}
+        {/*        e.preventDefault();*/}
+        {/*        handleRouteWithAuth(ROUTES.BOOK_REGISTER);*/}
+        {/*        setIsOpenMobileNav(false);*/}
+        {/*      }}*/}
+        {/*    >*/}
+        {/*      책 등록*/}
+        {/*    </Link>*/}
+        {/*    <Link*/}
+        {/*      href={'#'}*/}
+        {/*      className="btn btn-ghost"*/}
+        {/*      onClick={(e) => {*/}
+        {/*        e.preventDefault();*/}
+        {/*        handleRouteWithAuth(ROUTES.MY_BOOKSHELF);*/}
+        {/*        setIsOpenMobileNav(false);*/}
+        {/*      }}*/}
+        {/*    >*/}
+        {/*      나의 서재*/}
+        {/*    </Link>*/}
+        {/*    /!*<Link*!/*/}
+        {/*    /!*  href={'#'}*!/*/}
+        {/*    /!*  className="btn btn-ghost"*!/*/}
+        {/*    /!*  onClick={(e) => {*!/*/}
+        {/*    /!*    e.preventDefault();*!/*/}
+        {/*    /!*    handleRouteWithAuth(ROUTES.PROFILE);*!/*/}
+        {/*    /!*    setIsOpenMobileNav(false);*!/*/}
+        {/*    /!*  }}*!/*/}
+        {/*    /!*>*!/*/}
+        {/*    /!*  내 정보*!/*/}
+        {/*    /!*</Link>*!/*/}
+        {/*  </div>*/}
+        {/*)}*/}
       </header>
       {showLogoutModal && (
         <LogoutModal
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        secondary: {
+        primary: {
           50: '#FFFBEB',
           100: '#FEF3C7',
           200: '#FDE68A',
@@ -21,7 +21,7 @@ module.exports = {
           800: '#92400E',
           900: '#78350F',
         },
-        primary: {
+        secondary: {
           50: '#F8FAFC',
           100: '#F1F5F9',
           200: '#E2E8F0',
@@ -30,7 +30,7 @@ module.exports = {
           500: '#64748B',
           600: '#475569',
           700: '#334155',
-          800: '#1E293B', // 헤더/푸터 배경
+          800: '#1E293B',
           900: '#0F172A',
         },
         gray: {
