7abe9941b8aeaaa80d139f20fc706e8bca29f9b6
SJ0826 <ikosdu60@gmail.com>
2025-07-11 11:04:15 +0900

    KHF25-257 반응형 푸터 컴포넌트 제작

--- src/app/(main)/book/register/page.tsx
+++ src/app/(main)/book/register/page.tsx
@@ -1,8 +1,8 @@
-import BookForm from '@/app/book/register/BookForm';
+import BookForm from '@/app/(main)/book/register/BookForm';
 
 export default function BookRegisterPage() {
   return (
-    <div className="container mx-auto max-w-[1024px] p-6 bg-base-100 ">
+    <div className="bg-base-100 container mx-auto max-w-[1024px] p-6">
       <BookForm />
     </div>
   );
